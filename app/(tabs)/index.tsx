8d4fa4d5339094f6face805f22dcb34bdbad0874
Homegnon <jauresolympio@gmail.com>
2026-04-18 01:44:53 +0200

    Add home page header with avatar, balance and icons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,38 @@
+import { HOME_BALANCE, HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
 import "@/global.css";
-import { Link } from "expo-router";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
 const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
+
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-5xl font-sans-extrabold ">Home</Text>
-      
-      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to onboarding
-      </Link>
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar"/>
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
+        <Image source={icons.add} className="home-add-icon  ml-8"/>
+      </View>
 
-      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to Sign in 
-      </Link>
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balence</Text>
 
-      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to Sign up
-      </Link>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">{formatCurrency (HOME_BALANCE.amount)}</Text>
 
-      
+          <Text className="home-balance-date">
+            {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+          </Text>
+        </View>
+
+      </View>
     </SafeAreaView>
    
   );
---
+++ components/ListHeading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const ListHeading = () => {
+  return (
+    <View>
+      <Text>ListHeading</Text>
+    </View>
+  )
+}
+
+export default ListHeading
